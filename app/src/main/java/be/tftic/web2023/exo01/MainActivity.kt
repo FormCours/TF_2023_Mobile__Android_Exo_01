5b7f03208845ce29b591521d5e2de32506e7a7a6
Aurélien <aurelien.strimelle@bstorm.be>
2023-07-26 10:29:49 +0200

    Initial commit

---
+++ app/src/main/java/be/tftic/web2023/exo01/MainActivity.kt
@@ -0,0 +1,13 @@
+package be.tftic.web2023.exo01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
